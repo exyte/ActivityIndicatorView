25bc58019d161295bf4ae0a4d764170e882ab856
Alisa Mylnikova <f3dm76@gmail.com>
2020-06-30 14:07:19 +0700

    Fix Xcode 12 build error

--- Source/ActivityIndicatorView.swift
+++ Source/ActivityIndicatorView.swift
@@ -124,20 +124,26 @@ public struct ActivityIndicatorView: View {
     }
 
     func createRotatingDotsIndicator() -> AnyView {
-        AnyView(GeometryReader { (geometry: GeometryProxy) in
+
+        func createSmallCircle(_ size: CGSize, _ index: Int) -> some View {
+            return Circle()
+                .frame(width: size.width / 5, height: size.height / 5)
+                .scaleEffect(!self.isAnimating ? 1 - CGFloat(index) / 5 : 0.2 + CGFloat(index) / 5)
+                .offset(y: size.width / 10 - size.height / 2)
+        }
+
+        let indicator = GeometryReader { (geometry: GeometryProxy) in
             ForEach(0..<5) { index in
                 Group {
-                    Circle()
-                        .frame(width: geometry.size.width / 5, height: geometry.size.height / 5)
-                        .scaleEffect(!self.isAnimating ? 1 - CGFloat(index) / 5 : 0.2 + CGFloat(index) / 5)
-                        .offset(y: geometry.size.width / 10 - geometry.size.height / 2)
+                    createSmallCircle(geometry.size, index)
                 }.frame(width: geometry.size.width, height: geometry.size.height)
                     .rotationEffect(!self.isAnimating ? .degrees(0) : .degrees(360))
                     .animation(Animation
                         .timingCurve(0.5, 0.15 + Double(index) / 5, 0.25, 1, duration: 1.5)
                         .repeatForever(autoreverses: false))
             }
-        })
+        }
+        return AnyView(indicator)
     }
 
     func createFlickeringDotsIndicator() -> AnyView {
