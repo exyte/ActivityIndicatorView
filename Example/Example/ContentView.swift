30d02e0f9741f377977a42fb440ed88e4028d94c
Daniil Manin <manindaniil@gmail.com>
2021-10-07 15:48:01 +0700

    Remove progress indicator Remove AnyViews Small code fixes

--- Example/Example/ContentView.swift
+++ Example/Example/ContentView.swift
@@ -10,17 +10,15 @@ import SwiftUI
 import ActivityIndicatorView
 
 struct ContentView: View {
-
+    
     @State private var showLoadingIndicator: Bool = true
-    @State private var progressForProgressBar: CGFloat = 0.0
-    private let timer = Timer.publish(every: 1 / 5, on: .main, in: .common).autoconnect()
     
     var body: some View {
-        AnyView(GeometryReader { (geometry: GeometryProxy) -> AnyView in
+        GeometryReader { geometry in
             let size = geometry.size.width / 5
             let spacing: CGFloat = 40.0
             
-            return AnyView(VStack {
+            VStack {
                 HStack {
                     Spacer()
                     VStack(spacing: spacing) {
@@ -42,9 +40,9 @@ struct ContentView: View {
                             .foregroundColor(.red)
                         Spacer()
                     }
-                    
+
                     Spacer()
-                    
+
                     VStack(spacing: spacing) {
                         Spacer()
                         ActivityIndicatorView(isVisible: self.$showLoadingIndicator, type: .opacityDots)
@@ -65,29 +63,8 @@ struct ContentView: View {
                     }
                     Spacer()
                 }
-                
-                ActivityIndicatorView(isVisible: self.$showLoadingIndicator, type: .progressBar($progressForProgressBar))
-                    .frame(height: 8.0)
-                    .foregroundColor(.red)
-                    .padding([.bottom, .horizontal], size)
-                    .onReceive(timer) { _ in
-                        switch progressForProgressBar {
-                        case ...0.3, 0.4...0.6:
-                            progressForProgressBar += 1 / 30
-                        case 0.3...0.4, 0.6...0.9:
-                            progressForProgressBar += 1 / 120
-                        case 0.9...0.99:
-                            progressForProgressBar = 1
-                        case 1.0...:
-                            progressForProgressBar = 0
-                        default:
-                            break
-                        }
-                    }
-                
-                Spacer()
-            })
-        })
+            }
+        }
     }
 }
 
--- Source/ActivityIndicatorView.swift
+++ Source/ActivityIndicatorView.swift
@@ -21,7 +21,6 @@ public struct ActivityIndicatorView: View {
         case growingArc(Color = .red)
         case growingCircle
         case gradient([Color], CGLineCap = .butt)
-        case progressBar(Binding<CGFloat>, Color = .clear)
     }
 
     @Binding var isVisible: Bool
@@ -33,30 +32,39 @@ public struct ActivityIndicatorView: View {
     }
 
     public var body: some View {
-        guard isVisible else { return AnyView(EmptyView()) }
-        switch type {
-        case .default:
-            return AnyView(DefaultIndicatorView())
-        case .arcs:
-            return AnyView(ArcsIndicatorView())
-        case .rotatingDots:
-            return AnyView(RotatingDotsIndicatorView())
-        case .flickeringDots:
-            return AnyView(FlickeringDotsIndicatorView())
-        case .scalingDots:
-            return AnyView(ScalingDotsIndicatorView())
-        case .opacityDots:
-            return AnyView(OpacityDotsIndicatorView())
-        case .equalizer:
-            return AnyView(EqualizerIndicatorView())
-        case .growingArc(let color):
-            return AnyView(GrowingArcIndicatorView(color: color))
-        case .growingCircle:
-            return AnyView(GrowingCircleIndicatorView())
-        case .gradient(let colors, let lineCap):
-            return AnyView(GradientIndicatorView(colors: colors, lineCap: lineCap))
-        case .progressBar(let progress, let backgroundColor):
-            return AnyView(ProgressBarView(progress: progress, backgroundColor: backgroundColor))
+        if isVisible {
+            indicator
+        } else {
+            EmptyView()
+        }
+    }
+    
+    // MARK: - Private
+    
+    private var indicator: some View {
+        ZStack {
+            switch type {
+            case .default:
+                DefaultIndicatorView()
+            case .arcs:
+                ArcsIndicatorView()
+            case .rotatingDots:
+                RotatingDotsIndicatorView()
+            case .flickeringDots:
+                FlickeringDotsIndicatorView()
+            case .scalingDots:
+                ScalingDotsIndicatorView()
+            case .opacityDots:
+                OpacityDotsIndicatorView()
+            case .equalizer:
+                EqualizerIndicatorView()
+            case .growingArc(let color):
+                GrowingArcIndicatorView(color: color)
+            case .growingCircle:
+                GrowingCircleIndicatorView()
+            case .gradient(let colors, let lineCap):
+                GradientIndicatorView(colors: colors, lineCap: lineCap)
+            }
         }
     }
 }
